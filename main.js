de35f90ec46fd1fb99bb26fb2dd8b5f276e9c655
Emil <maxmudov2000@gmail.com>
2020-09-13 20:25:22 +0400

    Weather try/catch

--- main.js
+++ main.js
@@ -7,23 +7,31 @@ function getDate(from, count) {
     return result
 }
 
-
 function setTime() {
     let currentTime = getDate(16, 5)
     document.getElementById('clocks').innerHTML = currentTime;
 }
-
 
 function ChangeBackground() {
     let month = getDate(8, 2);
     document.getElementById('body').style.backgroundImage = `url(./img2/${month}.jpg)`
 }
 
-
 $("#settings").click(function () {
     $("#settings-menu").toggle(200);
 })
 
+function addMainFocus() {
+    let focus = $('#focusInput').val();
+
+    if (focus) {
+      console.log(focus);
+    }
+    
+    
+}
+
+document.getElementById('focusInput').addEventListener('blur', addMainFocus)
 
 setInterval(setTime, 1000);
 setInterval(ChangeBackground(), 60000);
--- weather.js
+++ weather.js
@@ -1,5 +1,14 @@
-(async function () {
-    let response = await fetch('https://api.openweathermap.org/data/2.5/weather?id=587084&appid=6d7bd30481e1e7bc7b798afe1eee644b');
+'use strict'
+
+let weather = async function () {
+    let response;
+    try {
+        response = await fetch('https://api.openweathermap.org/data/2.5/weather?id=587084&appid=6d7bd30481e1e7bc7b798afe1eee644b');
+    } catch {
+        console.log('Cant fetch API Open Weather Map. Retrying...');
+        weather();
+        return;
+    }
 
     response.json().then((result) => {
         document.getElementById('weather_city').innerText = result.name;
@@ -7,4 +16,7 @@
     }).catch(() => {
         console.log('Cant fetch API Open Weather Map. Check your connection');
     });
-})();
+};
+
+weather();
+setInterval(weather, 600000);
