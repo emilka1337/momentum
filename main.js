2e03645de64c550c50bc5602b304413369739b77
Emil <maxmudov2000@gmail.com>
2020-09-13 21:57:52 +0400

    Quotes added

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function getDate(from, count) {
     return result
 }
 
+let getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
 function setTime() {
     let currentTime = getDate(16, 5)
     document.getElementById('clocks').innerHTML = currentTime;
@@ -50,9 +52,39 @@ function striketroughMainFocus() {
     }
 }
 
+function setQuote() {
+    let quotes = [
+        {
+            quote: '“The world breaks everyone and afterward many are strong at the broken places.”',
+            author: 'Emest Hemingway'
+        },
+        {
+            quote: '“If your dreams don’t scare you, they are too small”',
+            author: 'Richard Branson'
+        },
+        {
+            quote: '“Good decisions come from experience. Experience comes from making bad decisions”',
+            author: 'Mark Twain'
+        },
+        {
+            quote: '“Life is a storm I don’t mind chasing”',
+            author: 'Clinton Sammy Jr'
+        },
+        {
+            quote: "“Dreams don't work unless you do”",
+            author: 'John C. Maxwell'
+        }
+    ];
+    let random = getRandom(0, quotes.length);
+
+    document.getElementById('quote').innerText = quotes[random].quote;
+    document.getElementById('quoteAuthor').innerText = quotes[random].author;
+}setQuote();
+
 document.getElementById('focusInput').addEventListener('blur', addMainFocus);
 document.getElementById('mainFocusDelete').addEventListener('click', removeMainFocus);
 document.getElementById('mainFocusCheckbox').addEventListener('click', striketroughMainFocus);
 
 setInterval(setTime, 1000);
 setInterval(ChangeBackground(), 60000);
+setInterval(setQuote, 120000);
